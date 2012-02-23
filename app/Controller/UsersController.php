b7fd4eefc1467b7ae70ac2f036f14f7a47285b85
Erik Knutson <erik.the.red.viking@gmail.com>
2012-02-23 15:08:57 -0600

    Added authent to mhsbake

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,109 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Users Controller
+ *
+ * @property User $User
+ */
+class UsersController extends AppController {
+	
+	
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('add', 'logout');
+	}
+	
+	public function login() {
+		if ($this->Auth->login()) {
+		    $this->redirect($this->Auth->redirect());
+		} else {
+		    $this->Session->setFlash(__('Invalid username or password, try again'));
+		}
+	}
+
+	public function logout() {
+		$this->redirect($this->Auth->logout());
+	}
+
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->User->recursive = 0;
+		$this->set('users', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		$this->set('user', $this->User->read(null, $id));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if ($this->User->save($this->request->data)) {
+				$this->flash(__('User saved.'), array('action' => 'index'));
+			} else {
+			}
+		}
+	}
+
+/**
+ * edit method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->User->save($this->request->data)) {
+				$this->flash(__('The user has been saved.'), array('action' => 'index'));
+			} else {
+			}
+		} else {
+			$this->request->data = $this->User->read(null, $id);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->User->delete()) {
+			$this->flash(__('User deleted'), array('action' => 'index'));
+		}
+		$this->flash(__('User was not deleted'), array('action' => 'index'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
---
+++ app/Model/User.php
@@ -0,0 +1,75 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * User Model
+ *
+ */
+class User extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name_last';
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name_last' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'name_first' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'username' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'password' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'role' => array(
+                        'valid' => array(
+                            'rule' => array('inList', array('admin', 'staff', 'basic')),
+                            'message' => 'Please enter a valid role',
+                            'allowEmpty' => false
+                        )
+                )
+	);
+        
+    public function beforeSave() {
+        if (isset($this->data[$this->alias]['password'])) {
+            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+        }
+        return true;
+    }
+}
