cd500b2cad09c4ac2d1e21bd47fad2a6ac943d9f
Erik Knutson <erik.the.red.viking@gmail.com>
2012-03-02 21:02:45 -0600

    Added a basic ACL functionality.  See email for more details

--- app/Controller/NewspaperReelsController.php
+++ app/Controller/NewspaperReelsController.php
@@ -6,6 +6,67 @@ App::uses('AppController', 'Controller');
  * @property NewspaperReel $NewspaperReel
  */
 class NewspaperReelsController extends AppController {
+	
+	/*
+	 *	beforeFilter() runs before its internal script before
+	 *	the action function (such as add, delete, or index) starts.
+	 *	Here, we add string names to the array of actions allowed
+	 *	without authorization.
+	 */
+	public function beforeFilter() {
+	    parent::beforeFilter();
+	    // Give some permissions that don't depend on authentication, such
+	    // as for logging in and logging out. Send an array as a parameter
+	    // if allowing more than one action. If allow passes no parameters,
+	    // authorization is given to all actions.
+	    //$this->Auth->allow();
+	}
+	
+	
+	/*
+	 *	isAuthorized(), as the UsersController level, makes additional
+	 *	checks for authentication.  Looking below, you see that several
+	 *	cases are considered:
+	 *		1. user session id and user id do not match
+	 *		2. $user['role'] is considered for each role, returning
+	 *		   true if the role is authorized to request action and
+	 *		   false if not authorized
+	 *		3. no user roles were matched, so authentication is not
+	 *		   given
+	 *	Checks for an action can be cirmcumvented completely by adding
+	 *	to $this->Auth->allow(ADD_TO_ARRAY) in beforeFilter().
+	 *	@precondition - this username and password checked
+	 *	@postcondition -  authorization pass/fails if returns true/false
+	 */
+	public function isAuthorized($user) {
+		
+		// Admin permissions [see also the beforeFilter()]
+		if ($user['role'] == 'admin') {
+			return true; // every action request authorized
+		}
+		
+		// Staff permissions [see also the beforeFilter()]
+		if ($user['role'] == 'staff') {
+			if (in_array($this->action, array('expanded',
+							  'index', 'view',
+							  'add', 'edit',
+							  'delete'))) {
+				return true; // action request authorized
+			}
+			return false; // action request not authorized
+		}
+		
+		// Basic permissions [see also the beforeFilter()]
+		if ($user['role'] == 'basic') {
+			if (in_array($this->action, array('expanded',
+							  'index', 'view'))) {
+				return true; // action request authorized
+			}
+			return false; // action request not authorized
+		}
+		
+		return false; // action request not authorized - unknown user 
+	}
 
 
 /**
